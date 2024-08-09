01cba8a0b464c838248a6f9c5f71b0c682fb278d
Axel Brisse <axel.brisse@gmail.com>
2024-08-09 13:33:59 +0200

    The Ray Tracer Challenger 082/268

--- src/objects/mod.rs
+++ src/objects/mod.rs
@@ -2,4 +2,14 @@ mod sphere;
 
 pub use sphere::Sphere;
 
+// use crate::Float;
+
+// trait Object {}
+
+// TODO: automate Intersection.object
+// struct Intersection {
+//     pub t: Float,
+//     pub object: &Object,
+// }
+
 // TODO: Hit or Intersect trait
