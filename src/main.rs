7c039f54eeaa9450ce4502dc83e4fe689bebacdb
Axel Brisse <axel.brisse@gmail.com>
2024-08-09 02:50:30 +0200

    .

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,5 @@
-use rt::Tuple;
-
 // TODO: projectile launcher from chapter 1
 
 fn main() {
-    println!("{:?}", Tuple::new_point(4.0, -4.0, 3.0));
-    println!("{:?}", Tuple::new_vector(-4.0, 4.0, -3.0));
+    println!("ooga");
 }
