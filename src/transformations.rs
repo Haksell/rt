fb9f58e130fd0560d1e21c187090449adae542cd
Axel Brisse <axel.brisse@gmail.com>
2024-08-09 10:52:39 +0200

    transformations::scaling

--- src/transformations.rs
+++ src/transformations.rs
@@ -10,19 +10,23 @@ pub fn translation(x: Float, y: Float, z: Float) -> Matrix<4> {
     ])
 }
 
-pub fn scaling(s: Float) -> Matrix<4> {
+pub fn scaling(x: Float, y: Float, z: Float) -> Matrix<4> {
     Matrix::new(&[
-        [s, 0.0, 0.0, 0.0],
-        [0.0, s, 0.0, 0.0],
-        [0.0, 0.0, s, 0.0],
+        [x, 0.0, 0.0, 0.0],
+        [0.0, y, 0.0, 0.0],
+        [0.0, 0.0, z, 0.0],
         [0.0, 0.0, 0.0, 1.0],
     ])
 }
 
+pub fn scaling_constant(s: Float) -> Matrix<4> {
+    scaling(s, s, s)
+}
+
 #[cfg(test)]
 mod tests {
-    use super::translation;
-    use crate::{transformations::scaling, Tuple};
+    use super::{scaling, scaling_constant, translation};
+    use crate::Tuple;
 
     #[test]
     fn test_translation() {
@@ -49,17 +53,37 @@ mod tests {
     #[test]
     fn test_scaling() {
         assert_eq!(
-            scaling(0.5) * Tuple::new_point(-3.0, 4.0, 5.0),
+            scaling(2.0, 3.0, 4.0) * Tuple::new_point(-4.0, 6.0, 8.0),
+            Tuple::new_point(-8.0, 18.0, 32.0)
+        );
+        assert_eq!(
+            scaling(2.0, 3.0, 4.0) * Tuple::new_vector(-4.0, 6.0, 8.0),
+            Tuple::new_vector(-8.0, 18.0, 32.0)
+        );
+        assert_eq!(
+            scaling(2.0, 3.0, 4.0).inverse() * Tuple::new_point(-4.0, 6.0, 8.0),
+            Tuple::new_point(-2.0, 2.0, 2.0)
+        );
+        assert_eq!(
+            scaling(-1.0, 1.0, 1.0) * Tuple::new_point(-4.0, 6.0, 8.0),
+            Tuple::new_point(4.0, 6.0, 8.0)
+        );
+    }
+
+    #[test]
+    fn test_scaling_constant() {
+        assert_eq!(
+            scaling_constant(0.5) * Tuple::new_point(-3.0, 4.0, 5.0),
             Tuple::new_point(-1.5, 2.0, 2.5)
         );
         assert_eq!(
-            scaling(-2.5) * Tuple::new_point(-3.0, 4.0, 5.0),
+            scaling_constant(-2.5) * Tuple::new_point(-3.0, 4.0, 5.0),
             Tuple::new_point(7.5, -10.0, -12.5)
         );
         assert_eq!(
-            scaling(1.0) * Tuple::new_point(-3.0, 4.0, 5.0),
+            scaling_constant(1.0) * Tuple::new_point(-3.0, 4.0, 5.0),
             Tuple::new_point(-3.0, 4.0, 5.0)
         );
-        assert_eq!(scaling(4.2) * Tuple::zero(), Tuple::zero());
+        assert_eq!(scaling_constant(4.2) * Tuple::zero(), Tuple::zero());
     }
 }
