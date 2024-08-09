5ffa6a797e6512d6b00c4a84da9af92f41286a0c
Axel Brisse <axel.brisse@gmail.com>
2024-08-09 10:39:15 +0200

    transformations::translation

--- src/transformations.rs
+++ src/transformations.rs
@@ -21,6 +21,10 @@ mod tests {
             translation(5.0, -3.0, 2.0) * Tuple::new_point(-3.0, 4.0, 5.0),
             Tuple::new_point(2.0, 1.0, 7.0)
         );
+        assert!(
+            (translation(5.0, -3.0, 2.0).inverse() * Tuple::new_point(-3.0, 4.0, 5.0))
+                .is_close(&Tuple::new_point(-8.0, 7.0, 3.0))
+        );
         assert_eq!(
             translation(0.5, -3.25, 7.0) * Tuple::new_point(0.0, 0.0, 0.0),
             Tuple::new_point(0.5, -3.25, 7.0)
@@ -29,5 +33,7 @@ mod tests {
             translation(0.0, 0.0, 0.0) * Tuple::new_point(-3.125, 4.0, 5.0),
             Tuple::new_point(-3.125, 4.0, 5.0)
         );
+        let v = Tuple::new_vector(-3.0, 4.0, 5.0); // not affected by translation
+        assert_eq!(translation(5.0, -3.0, 2.0) * v.clone(), v);
     }
 }
