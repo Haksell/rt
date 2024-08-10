14a4ff13bc3cc46e8cb1eb7ee8005f47a5d514b1
Axel Brisse <axel.brisse@gmail.com>
2024-08-10 04:58:07 +0200

    Object and Intersection wip

--- src/objects/mod.rs
+++ src/objects/mod.rs
@@ -1,15 +1,31 @@
 mod sphere;
 
+use std::fmt::Debug;
+
+use crate::{Float, Ray};
 pub use sphere::Sphere;
 
-// use crate::Float;
-
-// trait Object {}
-
 // TODO: automate Intersection.object
-// struct Intersection {
-//     pub t: Float,
-//     pub object: &Object,
-// }
+// TODO: no lifetime?
+#[derive(Debug)]
+pub struct Intersection<'a> {
+    pub object: &'a dyn Object,
+    pub t: Float,
+}
 
-// TODO: Hit or Intersect trait
+impl<'a> Intersection<'a> {
+    fn new(object: &'a dyn Object, t: Float) -> Self {
+        Self { object, t }
+    }
+}
+
+// turbo sus
+impl<'a> PartialEq for Intersection<'a> {
+    fn eq(&self, other: &Self) -> bool {
+        self.t == other.t && std::ptr::addr_eq(self.object, other.object)
+    }
+}
+
+pub trait Object: Debug {
+    fn intersect(&self, ray: &Ray) -> Vec<Intersection>;
+}
