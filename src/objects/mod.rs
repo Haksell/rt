8890e83c8edc4e16307386ed798f80cb0f9ab200
Axel Brisse <axel.brisse@gmail.com>
2024-08-10 05:14:24 +0200

    objects::hit

--- src/objects/mod.rs
+++ src/objects/mod.rs
@@ -29,3 +29,57 @@ impl<'a> PartialEq for Intersection<'a> {
 pub trait Object: Debug {
     fn intersect(&self, ray: &Ray) -> Vec<Intersection>;
 }
+
+pub fn hit<'a>(intersections: &'a [Intersection]) -> Option<&'a Intersection<'a>> {
+    intersections
+        .iter()
+        .filter(|&i| i.t > 0.0)
+        .min_by(|a, b| a.t.partial_cmp(&b.t).unwrap())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{hit, Intersection, Sphere};
+
+    #[test]
+    fn test_hit_all_positive() {
+        let sphere = Sphere::new();
+        let intersections = vec![
+            Intersection::new(&sphere, 4.0),
+            Intersection::new(&sphere, 6.0),
+        ];
+        assert_eq!(hit(&intersections), Some(&Intersection::new(&sphere, 4.0)));
+    }
+
+    #[test]
+    fn test_hit_one_positive() {
+        let sphere = Sphere::new();
+        let intersections = vec![
+            Intersection::new(&sphere, -1.0),
+            Intersection::new(&sphere, 1.0),
+        ];
+        assert_eq!(hit(&intersections), Some(&Intersection::new(&sphere, 1.0)));
+    }
+
+    #[test]
+    fn test_hit_all_negative() {
+        let sphere = Sphere::new();
+        let intersections = vec![
+            Intersection::new(&sphere, -6.0),
+            Intersection::new(&sphere, -4.0),
+        ];
+        assert_eq!(hit(&intersections), None);
+    }
+
+    #[test]
+    fn test_hit_more() {
+        let sphere = Sphere::new();
+        let intersections = vec![
+            Intersection::new(&sphere, 5.0),
+            Intersection::new(&sphere, 7.0),
+            Intersection::new(&sphere, -3.0),
+            Intersection::new(&sphere, 2.0),
+        ];
+        assert_eq!(hit(&intersections), Some(&Intersection::new(&sphere, 2.0)));
+    }
+}
